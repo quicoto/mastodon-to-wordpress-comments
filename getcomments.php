dd60402d1b3dc296c483752a1785fef634b03014
Ricard Torres <ricard.torres@netcentric.biz>
2018-10-17 14:26:05 +0200

    Bring back this filter see if the emojis are stored correctly

--- getcomments.php
+++ getcomments.php
@@ -257,7 +257,8 @@ foreach ( $posts_array as $post ) {
                     // Does not exist, create a new parent comment
                     // https://codex.wordpress.org/Function_Reference/wp_new_comment
                     $toot_url = $status['url'];
-                    $content = removeTag($status['content'], 'span') . '<br><br><a href="'. $toot_url .'" rel="nofollow">Original Toot</a>';
+                    $clean_content = removeTag($status['content'], 'span');
+                    $content = apply_filters('the_content', $clean_content) . '<br><br><a href="'. $toot_url .'" rel="nofollow">Original Toot</a>';
                     $commentdata = array(
                         'comment_post_ID' => $post->ID,
                         'comment_author' => $status['account']['display_name'],
@@ -293,7 +294,8 @@ foreach ( $posts_array as $post ) {
                             // No replies with this ID
                             // Let's add the comment as reply to the main one
                             $toot_url = $reply['url'];
-                            $content = removeTag($reply['toot'], 'span') . '<br><br><a href="'. $toot_url .'" rel="nofollow">Original Toot</a>';
+                            $clean_content = removeTag($reply['toot'], 'span');
+                            $content = apply_filters('the_content', $clean_content) . '<br><br><a href="'. $toot_url .'" rel="nofollow">Original Toot</a>';
                             $commentdata = array(
                                 'comment_post_ID' => $post->ID,
                                 'comment_author' => $reply['author']['display_name'],
